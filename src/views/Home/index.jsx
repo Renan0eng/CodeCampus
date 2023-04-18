45f55d7613906146e646b1205fd833b185090062
Renan Nardi <renan_terra_rica@hotmail.com.br>
2023-04-18 22:14:03 +0000

    homeee

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -100,7 +100,7 @@ export default function FeedExample() {
               <MenuIcon />
             </IconButton>
             <Typography component="h1" fontWeight="xl" minWidth={100}>
-              Home
+              Homeee
             </Typography>
           </Box>
           <Box sx={{ display: 'flex', flexDirection: 'row', gap: 1.5 }}>
